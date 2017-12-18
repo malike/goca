07c55ab921578d13df466cf1fcb6a48aac10b935
malike <st.malike@gmail.com>
2017-12-18 23:43:01 +0000

    implmentation for adjancencylist representation of graphs

--- graphs/adjancencyset/adjancencyset.go
+++ graphs/adjancencyset/adjancencyset.go
@@ -7,16 +7,41 @@ type AdjacencySet struct {
 	GraphType graphs.GraphType
 }
 
-func (adjacencySet AdjacencySet) GetAdjacentVerticesNodesForVertex(vertex int) []int {
-	return []int{1, 2}
+func (adjacencySet AdjacencySet) Init() {
+
+}
+
+func (adjacencySet AdjacencySet) AddEdge(vertexOne int, vertexTwo int) error {
+	return nil
+}
+
+func (adjacencySet AdjacencySet) AddEdgeWithWeight(vertexOne int, vertexTwo int, weight int) error {
+	return nil
+}
+
+func (adjacencySet AdjacencySet) RemoveEdge(vertexOne int, vertexTwo int) error {
+
+	return nil
+}
+
+func (adjacencySet AdjacencySet) HasEdge(vertexOne int, vertexTwo int) bool {
+	return nil
+}
+
+func (adjacencySet AdjacencySet) GetGraphType() graphs.GraphType {
+	return nil
+}
+
+func (adjacencySet AdjacencySet) GetAdjacentVerticesNodesForVertex(vertex int) map[int]bool {
+	return nil
 }
 
 func (adjacencySet AdjacencySet) GetWeightOfEdge(vertexOne int, vertexTwo int) int {
-	return 1
+	return nil
 }
 
 func (adjacencySet AdjacencySet) GetNumberOfVertices() int {
-	return 1
+	return nil
 }
 
 func (adjacencySet AdjacencySet) GetIndegreeForVertex(vertex int) int {
@@ -25,24 +50,4 @@ func (adjacencySet AdjacencySet) GetIndegreeForVertex(vertex int) int {
 
 func (adjacencySet AdjacencySet) GetOutdegreeForVertex(vertex int) int {
 	return 1
-}
-
-func (adjacencySet AdjacencySet) AddEdge(vertexOne int, vertexTwo int) {
-	return 1
-}
-
-func (adjacencySet AdjacencySet) AddEdgeWithWeight(vertexOne int, vertexTwo int, weight int) {
-	return 1
-}
-
-func (adjacencySet AdjacencySet) RemoveEdge(vertexOne int, vertexTwo int) {
-
-}
-
-func (adjacencySet AdjacencySet) HasEdge(vertexOne int, vertexTwo int) bool {
-	return 1
-}
-
-func (adjacencySet AdjacencySet) GetGraphType() graphs.GraphType {
-	return graphs.DIRECTED
 }
--- graphs/adjancencyset/adjancencyset_test.go
+++ graphs/adjancencyset/adjancencyset_test.go
@@ -1,12 +1,58 @@
 package adjancencyset
 
-import "testing"
-
-func TestAdjacencySet_GetAdjacentVerticesNodesForVertex(t *testing.T) {
+func TestAdjacencySet_AddEdgeDirectedGreaterThanVertex(t *testing.T) {
 
 }
 
-func TestAdjacencySet_GetWeightOfEdge(t *testing.T) {
+func TestAdjacencySet_AddEdgeDirected(t *testing.T) {
+
+}
+
+func TestAdjacencySet_AddEdgeUndirected(t *testing.T) {
+
+}
+
+func TestAdjacencySet_AddEdgeWeightDirectedGreaterThanVertex(t *testing.T) {
+
+}
+
+func TestAdjacencySet_AddEdgeWithWeightDirected(t *testing.T) {
+
+}
+
+func TestAdjacencySet_AddEdgeWithWeightUnDirected(t *testing.T) {
+
+}
+
+func TestAdjacencySet_RemoveEdgeDirected(t *testing.T) {
+
+}
+
+func TestAdjacencySet_RemoveEdgeUnDirected(t *testing.T) {
+
+}
+
+func TestAdjacencySet_HasEdgeDirected(t *testing.T) {
+
+}
+
+func TestAdjacencySet_HasEdgeUndirected(t *testing.T) {
+
+}
+
+func TestAdjacencySet_GetGraphType(t *testing.T) {
+
+}
+
+func TestAdjacencySet_GetAdjacentVerticesNodesForVertexUndirected(t *testing.T) {
+
+}
+
+func TestAdjacencySet_GetWeightOfEdgeDirected(t *testing.T) {
+
+}
+
+func TestAdjacencySet_GetWeightOfEdgeUndirected(t *testing.T) {
 
 }
 
@@ -19,25 +65,5 @@ func TestAdjacencySet_GetIndegreeForVertex(t *testing.T) {
 }
 
 func TestAdjacencySet_GetOutdegreeForVertex(t *testing.T) {
-
-}
-
-func TestAdjacencySet_AddEdge(t *testing.T) {
-
-}
-
-func TestAdjacencySet_AddEdgeWithWeight(t *testing.T) {
-
-}
-
-func TestAdjacencySet_HasEdge(t *testing.T) {
-
-}
-
-func TestAdjacencySet_RemoveEdge(t *testing.T) {
-
-}
-
-func TestAdjacencySet_GetGraphType(t *testing.T) {
 
 }
