3553ebfb87a11c84c8e8dba2a41c27727222d93b
Samuel Addico <samuel.addico@dreamoval.com>
2018-01-03 12:36:50 +0000

    Added algorithm implementations for Merge and Quick Sorts

--- sorting/mergesort_test.go
+++ sorting/mergesort_test.go
@@ -1,1 +1,12 @@
 package sorting
+
+import "testing"
+
+func TestMergeSort_MergeSort(t *testing.T) {
+	actualArray := []int{9,5,7,3,6,2,1}
+	expectedArray := MergeSort(actualArray)
+
+	if expectedArray[len(actualArray) - 1] != 9  {
+		t.Fatalf("Expected %v but got %v", expectedArray, actualArray)
+	}
+}
--- sorting/quicksort_test.go
+++ sorting/quicksort_test.go
@@ -1,1 +1,12 @@
 package sorting
+
+import "testing"
+
+func TestQuickSort_QuickSort(t *testing.T) {
+	actualArray := []int{9,5,7,3,6,2,1}
+	expectedArray := QuickSort(actualArray)
+
+	if expectedArray[len(actualArray) - 1] != 9  {
+		t.Fatalf("Expected %v but got %v", expectedArray, actualArray)
+	}
+}
